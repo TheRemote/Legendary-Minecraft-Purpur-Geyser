fe94a31c58d035010717d2fc27c5384daea6408b
James Chambers <05jchambers@gmail.com>
2025-07-24 17:07:25 -0600

    Add build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
-#docker build -t 05jchambers/legendary-minecraft-purpur-geyser:amd64 -f amd64.Dockerfile .
+docker build -t 05jchambers/legendary-minecraft-purpur-geyser:amd64 -f amd64.Dockerfile .
 docker push 05jchambers/legendary-minecraft-purpur-geyser:amd64
 
-#docker build -t 05jchambers/legendary-minecraft-purpur-geyser:arm64v8 -f arm64v8.Dockerfile .
+docker build -t 05jchambers/legendary-minecraft-purpur-geyser:arm64v8 -f arm64v8.Dockerfile .
 docker push 05jchambers/legendary-minecraft-purpur-geyser:arm64v8
 
-#docker build -t 05jchambers/legendary-minecraft-purpur-geyser:armv7 -f armv7.Dockerfile .
+docker build -t 05jchambers/legendary-minecraft-purpur-geyser:armv7 -f armv7.Dockerfile .
 docker push 05jchambers/legendary-minecraft-purpur-geyser:armv7
 
-#docker build -t 05jchambers/legendary-minecraft-purpur-geyser:ppc64le -f ppc64le.Dockerfile .
+docker build -t 05jchambers/legendary-minecraft-purpur-geyser:ppc64le -f ppc64le.Dockerfile .
 docker push 05jchambers/legendary-minecraft-purpur-geyser:ppc64le
 
-#docker build -t 05jchambers/legendary-minecraft-purpur-geyser:s390x -f s390x.Dockerfile .
+docker build -t 05jchambers/legendary-minecraft-purpur-geyser:s390x -f s390x.Dockerfile .
 docker push 05jchambers/legendary-minecraft-purpur-geyser:s390x
 
 ./manifest-tool-linux-amd64 push from-spec multi-arch-manifest.yaml
